ec9f107748e98b163789ff480bcde94ebb52bf90
Jose Manuel Adrian Iramain <jiramain@todo1.net>
2021-05-24 18:33:45 -0300

    R3 fix bug

--- Code/R3/script.js
+++ Code/R3/script.js
@@ -2,4 +2,16 @@ $(document).ready(() => {
     $("#videoModal").on('hidden.bs.modal', () => {
       $("#videoModal iframe").attr("src", $("#videoModal iframe").attr("src"));
     });
+  
+    $("#btn-modal").click(() => {
+      $("#modal").show();
+    });
+  
+    $("#btn-modal-close").click(() => {
+      $("#modal").hide();
+    });
   });
+  
+  function openModal() {
+  
+  }
